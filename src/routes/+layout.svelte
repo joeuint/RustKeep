a9d975516720bc7541bfa5e56dc22f3cad76a24f
joqwer <94891082+joqwer@users.noreply.github.com>
2023-08-16 02:08:31 -0400

    Unsaved database confirmation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,32 @@
   import { writable } from 'svelte/store';
   import type { Alert } from '$types/Alert';
   import type { Database } from '$types/Database';
+  import { getCurrent } from '@tauri-apps/api/window';
+  import { confirm } from '@tauri-apps/api/dialog';
 
   const alerts = writable<Alert>();
   const database = writable<Database>();
+  const unsaved = writable<boolean>(false);
 
   setContext('alerts', alerts);
   setContext('database', database);
+  setContext('unsaved', unsaved);
+
+  // Ask the user if they want to quit with unsaved changes
+  getCurrent().onCloseRequested(async (event) => {
+    if (unsaved) {
+      const quitAnyways = await confirm(
+        'Your changes are not saved. Are you sure you would like to quit?',
+        {
+          title: 'Unsaved Database',
+          type: 'warning',
+        }
+      );
+      if (!quitAnyways) {
+        event.preventDefault();
+      }
+    }
+  });
 </script>
 
 <NavHeader />
