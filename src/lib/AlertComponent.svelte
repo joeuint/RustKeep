b4935f5b42355fd99af7da443a60ac4621899123
joqwer <94891082+joqwer@users.noreply.github.com>
2023-09-11 20:10:37 -0400

    Improved Alert

--- src/lib/AlertComponent.svelte
+++ src/lib/AlertComponent.svelte
@@ -10,7 +10,9 @@
   const kindDisplay: string = getTitle(kind);
 </script>
 
-<div class="w-1/2 mx-auto mt-4 bg-red-600/40 p-2 rounded-lg">
+<div
+  class="w-1/2 mx-auto mt-4 bg-red-600/40 p-2 rounded-lg absolute left-[25vw]"
+>
   <h2 class="text-xl font-semibold">{kindDisplay}:</h2>
   <p><slot /></p>
 </div>
